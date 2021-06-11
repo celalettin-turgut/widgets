41527a02e9278e2f487101f69288cd74c778a7a6
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-11 20:46:57 +0200

    App.js, index.js, Accordion.js created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const App = () => {
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+
+const Accordion = () => {
+  return <div>I am Accordion</div>;
+};
+
+export default Accordion;
