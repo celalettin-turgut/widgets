c337fe49f0942a73ba14bbea956bb07cbc0a3fb1
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-12 15:06:55 +0200

    Search component inserted into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const data = [
+  {
+    title: "Never Give up!",
+    text: "Dont give up the fight. The life changes continously. So you should keep fighting!",
+  },
+  {
+    title: "Life is too short",
+    text: "Never break a heart as the life is too short. Take a coffee and enjoy the life with your loved ones!",
+  },
+  {
+    title: "Nothing is more importand than life itself",
+    text: "It is your life. Dont let them ruin your life. Just smile and let them go!",
+  },
+];
 
 const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion data={data} />
+      <hr />
+      <br />
+      <Search />
     </div>
   );
 };
