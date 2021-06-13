c31972afaf1ec3754d13b5e7dc05d16b562de221
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-13 13:51:14 +0200

    Search component to get API request from Wiki

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Input } from "antd";
+import { Input, List } from "antd";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
   const onChange = (event) => {
     setTerm(event.target.value);
   };
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      console.log(data.query.search);
+      setResults(data.query.search);
+    };
+    search();
+  }, [term]);
 
   return (
     <div>
@@ -19,7 +37,28 @@ const Search = () => {
         onChange={onChange}
         value={term}
       />
-      {console.log("Burasi render")}
+      <List
+        itemLayout='horizontal'
+        dataSource={results}
+        renderItem={(item) => (
+          <List.Item>
+            <List.Item.Meta
+              title={
+                <a
+                  target={`_blank`}
+                  href={`https://en.wikipedia.org?curid=${item.pageid}`}
+                >
+                  {item.title}
+                </a>
+              }
+              //this is a trick to omit the span elements in the text
+              description={
+                <span dangerouslySetInnerHTML={{ __html: item.snippet }}></span>
+              }
+            />
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
