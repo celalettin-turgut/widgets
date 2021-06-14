69032f3ed399c01871ae7ff2d2675c8f89a81d73
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-14 17:57:08 +0200

    configs

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,32 +1,38 @@
 import React from "react";
-import { Menu, Dropdown } from "antd";
+import { Menu, Dropdown, Button } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 
-const DropDown = ({ options }) => {
+const DropDown = ({ options, selected, setSelected }) => {
   const menu = (
-    <Menu>
-      {options.map((option, index) => {
-        return <Menu.Item key={index}>{option.label}</Menu.Item>;
-      })}
+    <Menu onClick={handleMenuClick}>
+      {options
+        .filter((option) => {
+          return selected.value !== option.value;
+        })
+        .map((option) => {
+          return (
+            <Menu.Item onClick={(e) => setSelected(option)} key={option.value}>
+              {option.label}
+            </Menu.Item>
+          );
+        })}
       {/* <Menu.Item key='0'>
         <a href='https://www.antgroup.com'>1st menu item</a>
       </Menu.Item>
-      <Menu.Item key='1'>
-        <a href='https://www.aliyun.com'>2nd menu item</a>
-      </Menu.Item>
-      <Menu.Divider />
-      <Menu.Item key='3'>3rd menu item</Menu.Item> */}
+     */}
     </Menu>
   );
 
+  function handleMenuClick(e) {
+    console.log("click", e);
+  }
+
   return (
-    <div>
-      <Dropdown overlay={menu} trigger={["click"]}>
-        <a className='ant-dropdown-link' onClick={(e) => e.preventDefault()}>
-          Click me <DownOutlined />
-        </a>
-      </Dropdown>
-    </div>
+    <Dropdown overlay={menu}>
+      <Button>
+        {selected.label} <DownOutlined />
+      </Button>
+    </Dropdown>
   );
 };
 
