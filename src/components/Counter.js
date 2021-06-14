a6af8875032a20706f6eab1b5b31c93a167ed882
Celalettin Turgut <celalettin.turgut@hotmail.com>
2021-06-14 17:55:25 +0200

    Counter component created

---
+++ src/components/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let intervalID = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  });
+
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>{count}</h1>
+    </div>
+  );
+};
+
+export default Counter;
